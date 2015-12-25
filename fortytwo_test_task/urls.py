29460c3c6a4507b6b8ffc4da8573fdeeba609f06
Paul Pukach <pavlopukach@gmail.com>
2015-12-25 16:00:27 +0200

    ticket:1 made redirect from root URL to /home/

--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic.base import RedirectView
 
 admin.autodiscover()
 
@@ -10,5 +11,10 @@ urlpatterns = patterns(
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^home/', include('apps.personal_info.urls'))
+    url(r'^home/', include('apps.personal_info.urls')),
+    url(
+        r'^$',
+        RedirectView.as_view(url='/home/', permanent=False),
+        name='index'
+    ),
 )
