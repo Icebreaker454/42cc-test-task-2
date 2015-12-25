4b4252dc9322da5f738ae5404a93461541c98bad
Paul Pukach <pavlopukach@gmail.com>
2015-12-25 17:19:00 +0200

    ticket:1 added tests for for the view that should return hardcoded data

--- apps/personal_info/tests.py
+++ apps/personal_info/tests.py
@@ -1,3 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""
+    This module contains functional tests for the
+    personal_info application.
+"""
+
+from django.core.urlresolvers import reverse
 from django.test import TestCase
 
 # Create your tests here.
@@ -8,7 +16,32 @@ class LandingPageTest(TestCase):
     This is a set of tests for the Home page
     """
     def setUp(self):
-        """
-        Initialize testing data
-        """
-        pass
+        """ Initialize testing data """
+        self.url = reverse('home')
+
+    def test_response_status_code(self):
+        """ Test whether the response status code is OK """
+        resp = self.client.get(self.url)
+        self.assertEqual(resp.status_code, 200)
+
+    def test_personal_data_on_page(self):
+        """ Test whether personal data is displayed on the page """
+        resp = self.client.get(self.url)
+
+        self.assertIn('object', resp.context)
+
+        self.assertIn('Paul', resp.content)
+        self.assertIn('Pukach', resp.content)
+        self.assertIn('25 Jun, 1996', resp.content)
+        self.assertIn('applied mathematician', resp.content)
+
+    def test_contact_data_on_page(self):
+        """ Test whether the contact data is displayed on the page """
+        resp = self.client.get(self.url)
+
+        self.assertIn('object', resp.context)
+
+        self.assertIn('pavlopukach@gmail.com', resp.content)
+        self.assertIn('icebreaker454@khavr.com', resp.content)
+        self.assertIn('shoker2506', resp.content)
+        self.assertIn('+380963699598', resp.content)
