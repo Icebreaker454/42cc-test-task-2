0ab359825d9a41fb51761e9c7dba817f0cdab4fc
Paul Pukach <pavlopukach@gmail.com>
2015-12-28 19:38:32 +0200

    ticket:3 Added tests for the WebRequest model;

--- apps/requests/tests.py
+++ apps/requests/tests.py
@@ -4,9 +4,12 @@
     This module contains functional tests for the
     requests application.
 """
+from datetime import datetime
 
 from django.core.urlresolvers import reverse
 from django.test import TestCase
+
+from apps.requests.models import WebRequest
 
 
 class RequestsTest(TestCase):
@@ -23,3 +26,24 @@ class RequestsTest(TestCase):
         self.assertContains(resp, 'Last 10 HTTP requests')
         self.assertContains(resp, 'www.somesite.com/path-1')
         self.assertContains(resp, 'POST')
+
+
+class WebRequestTest(TestCase):
+    """ This is a set of tests for the WebRequest model """
+    def setUp():
+        """ Prvide initial testing data """
+        WebRequest.objects.create(
+            path='www.test.com/test/',
+            date=datetime.now(),
+            method='GET'
+        )
+
+    def test_string_representation(self):
+        """ Test that the string representation match the standard """
+        self.assertEqual(
+            self.webrequest.__unicode__(),
+            "%s %s" % (
+                self.webrequest.method,
+                self.webrequest.path
+            )
+        )
